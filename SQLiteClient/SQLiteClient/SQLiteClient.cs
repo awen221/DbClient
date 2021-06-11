0accf1aab23212b679a11defff853c67fe3684ca
larry.lai <larry.lai@advantech.com>
2021-06-11 17:36:56 +0800

    新增SQLite類別

---
+++ SQLiteClient/SQLiteClient/SQLiteClient.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Data.SQLite;
+
+using DbClient;
+
+namespace SQLiteClient
+{
+    public class SQLiteClient : DbClient<SQLiteConnection, SQLiteCommand, SQLiteDataAdapter> 
+    {
+        public void ConnectionCreateFile(string filename) => SQLiteConnection.CreateFile(filename);
+    }
+}
